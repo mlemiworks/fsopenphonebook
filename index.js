14ce7d0c8a932ba5e939047856257a2c93aa0c85
Markus Lemiläinen <markus.lemilainen@edu.karelia.fi>
2024-08-29 17:33:21 +0300

    'uibuild'

--- index.js
+++ index.js
@@ -1,132 +1,132 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const Contact = require("./models/contact");
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const Contact = require('./models/contact')
+const app = express()
 
-app.use(express.json());
-app.use(cors());
-app.use(express.static("dist"));
+app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 
-morgan.token("body", (request) => {
-  return JSON.stringify(request.body);
-});
+morgan.token('body', (request) => {
+  return JSON.stringify(request.body)
+})
 
 // loggausta
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 // Hae kaikki
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   Contact.find({}).then((contacts) => {
-    response.json(contacts);
-  });
-});
+    response.json(contacts)
+  })
+})
 
 // Info sivu
-app.get("/info", (request, response) => {
+app.get('/info', (request, response) => {
   Contact.find({}).then((contacts) => {
-    const infoText = `<p>Phonebook has info for ${contacts.length} people</p>`;
-    const dateTime = Date();
-    response.send(infoText + dateTime);
-  });
-});
+    const infoText = `<p>Phonebook has info for ${contacts.length} people</p>`
+    const dateTime = Date()
+    response.send(infoText + dateTime)
+  })
+})
 
 // Yhteystieto haku id:llä
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Contact.findById(request.params.id)
     .then((contact) => {
-      response.json(contact);
+      response.json(contact)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // Yhteystiedon lisäys
-app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
   if (body.name.length === 0 || body.number.length === 0) {
     return response.status(400).json({
-      error: "number or name missing",
-    });
+      error: 'number or name missing',
+    })
   }
 
   const contact = new Contact({
     name: body.name,
     number: body.number,
-  });
+  })
 
   contact
     .save()
     .then((savedPerson) => {
-      response.json(savedPerson);
+      response.json(savedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // Yhteystiedon muokkaus
-app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
   const contact = {
     name: body.name,
     number: body.number,
-  };
+  }
 
   Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
     .then((updatedContact) => {
-      response.json(updatedContact);
+      response.json(updatedContact)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // Yhteystiedon poisto
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Contact.findByIdAndDelete(request.params.id)
-    .then((result) => {
-      response.status(204).end();
+    .then(() => {
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // Yhteystiedon muokkaus
 
 //portin kuuntelu
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
 
 //eli olematon route
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 // Väärämuotoinen id
 const errorHandler = (error, request, response, next) => {
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
     const customMessage = Object.values(error.errors)
       .map(
         (error) => `Invalid ${error.path}: "${error.value}" - ${error.message}`
       )
-      .join(", ");
-    return response.status(400).json({ error: customMessage });
+      .join(', ')
+    return response.status(400).json({ error: customMessage })
   }
 
-  next(error);
-};
+  next(error)
+}
 
 // Joku muu
 const generalErrorHandler = (error, request, response) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  return response.status(500).json({ error: "internal server error" });
-};
+  return response.status(500).json({ error: 'internal server error' })
+}
 
 // Routejen ja middlewarejen perään aina
-app.use(unknownEndpoint);
-app.use(errorHandler);
-app.use(generalErrorHandler);
+app.use(unknownEndpoint)
+app.use(errorHandler)
+app.use(generalErrorHandler)
--- models/contact.js
+++ models/contact.js
@@ -1,42 +1,42 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connectig to", url);
+console.log('connectig to', url)
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("connectedto MongoDB");
+  .then(() => {
+    console.log('connectedto MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB", error.message);
-  });
+    console.log('error connecting to MongoDB', error.message)
+  })
 
 const contactSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: [3, "It must be at least 3 characters"],
+    minlength: [3, 'It must be at least 3 characters'],
   },
   number: {
     type: String,
     validate: {
       validator: function (value) {
-        return /^(\d{2,3})-\d{5,}$/.test(value);
+        return /^(\d{2,3})-\d{5,}$/.test(value)
       },
       message:
-        "It must be atleast 8 character and in format xx-xxxxx or xxx-xxxxx",
+        'It must be atleast 8 character and in format xx-xxxxx or xxx-xxxxx',
     },
   },
-});
+})
 
-contactSchema.set("toJSON", {
+contactSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Contact", contactSchema);
+module.exports = mongoose.model('Contact', contactSchema)
