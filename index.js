d1554a75c2aa421d21523494f3b3131382898a65
Markus Lemiläinen <markus.lemilainen@edu.karelia.fi>
2024-08-29 16:40:06 +0300

    'uibuild'

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Contact = require("./models/contact");
 const app = express();
 
 app.use(express.json());
@@ -11,90 +13,120 @@ morgan.token("body", (request) => {
   return JSON.stringify(request.body);
 });
 
+// loggausta
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: "1",
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: "2",
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: "3",
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: "4",
-  },
-];
-
-const generateId = () => {
-  const maxId =
-    persons.length > 0 ? Math.max(...persons.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
-};
-
+// Hae kaikki
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Contact.find({}).then((contacts) => {
+    response.json(contacts);
+  });
 });
 
+// Info sivu
 app.get("/info", (request, response) => {
-  const infoText = `<p>Phonebook has info for ${persons.length} people</p>`;
-  const dateTime = Date();
-  response.send(infoText + dateTime);
+  Contact.find({}).then((contacts) => {
+    const infoText = `<p>Phonebook has info for ${contacts.length} people</p>`;
+    const dateTime = Date();
+    response.send(infoText + dateTime);
+  });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+// Yhteystieto haku id:llä
+app.get("/api/persons/:id", (request, response, next) => {
+  Contact.findById(request.params.id)
+    .then((contact) => {
+      response.json(contact);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
-});
-
-app.post("/api/persons", (request, response) => {
+// Yhteystiedon lisäys
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (body.name.length === 0 || body.number.length === 0) {
     return response.status(400).json({
       error: "number or name missing",
     });
-  } else if (persons.find((person) => person.name === body.name)) {
-    return response.status(400).json({
-      error: "contact with such name already exists",
-    });
   }
 
-  const person = {
+  const contact = new Contact({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  };
+  });
 
-  persons = persons.concat(person);
-  response.json(person);
-  console.log(persons);
+  contact
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-const PORT = process.env.PORT || 3001;
+// Yhteystiedon muokkaus
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  const contact = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
+    .then((updatedContact) => {
+      response.json(updatedContact);
+    })
+    .catch((error) => next(error));
+});
+
+// Yhteystiedon poisto
+app.delete("/api/persons/:id", (request, response, next) => {
+  Contact.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+// Yhteystiedon muokkaus
+
+//portin kuuntelu
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+//eli olematon route
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+// Väärämuotoinen id
+const errorHandler = (error, request, response, next) => {
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    const customMessage = Object.values(error.errors)
+      .map(
+        (error) => `Invalid ${error.path}: "${error.value}" - ${error.message}`
+      )
+      .join(", ");
+    return response.status(400).json({ error: customMessage });
+  }
+
+  next(error);
+};
+
+// Joku muu
+const generalErrorHandler = (error, request, response) => {
+  console.error(error.message);
+
+  return response.status(500).json({ error: "internal server error" });
+};
+
+// Routejen ja middlewarejen perään aina
+app.use(unknownEndpoint);
+app.use(errorHandler);
+app.use(generalErrorHandler);
